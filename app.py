f87534e2b46cc06b8d04a12e329f339fdcc4b41a
eliel otro <eligonzamcraf@gmail.com>
2025-08-08 12:11:47 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/GPS')
+def gps():
+    return render_template('GPS.html')
+
+@app.route('/datos')
+def datos():
+    return render_template('datos.html')
+
+@app.route('/control')
+def control():
+    return render_template('control.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
